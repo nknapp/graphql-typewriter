cb4dfa6c8cdb4eae6f28ce7691b0c326ae155b12
Mario Siegenthaler <mario.siegenthaler@linkyard.ch>
2017-01-04 00:01:22 +0100

    Add the missing default scalars Float and ID.

--- test/schemas/scalars.ts
+++ test/schemas/scalars.ts
@@ -3,5 +3,7 @@ export namespace schema {
         stringField: string | Promise<string> | { (): string } | { (): Promise<string> }
         booleanField: boolean | Promise<boolean> | { (): boolean } | { (): Promise<boolean> }
         intField: number | Promise<number> | { (): number } | { (): Promise<number> }
+        floatField: number | Promise<number> | { (): number } | { (): Promise<number> }
+        idField: string | Promise<string> | { (): string } | { (): Promise<string> }
     }
 }

73f6ebb37d6235607542ee24b4b9fa06453f0e2f
Nils Knappmeier <github@knappi.org>
2017-01-08 01:03:41 +0100

    fixup: test-runner in Node 4 fixed
    - Add dummy-params for dynamic arguments
    - Replace "no-unused-variable" rule by compiler-parameter "--noUnusedLocals" in npm-scripts

--- src/renderTag.ts
+++ src/renderTag.ts
@@ -14,9 +14,15 @@ export const OMIT_NEXT_NEWLINE = {
  * and expands the current indent on multiline substitions
  * @see http://exploringjs.com/es6/ch_template-literals.html
  * @param array the input array
- * @param args
+ * @param a1 string substitution
+ * @param a2 string substitution
+ * @param a2 string substitution
+ * @param a3 string substitution
+ * @param a4 string substitution
+ * @param a5 string substitution
+ * @param a6 string substitution
  */
-export function source(array /* dynamic args */) {
+export function source(array, a1, a2?, a3?, a4?, a5?, a6? /* dynamic args */) {
     const args = Array.prototype.slice.call(arguments, 1)
     let result = array[0]
     for (let i = 0; i < args.length; i++) {

f0b1ce3cd9fee4bcffeedd203fbffa7ea2b91a82
Nils Knappmeier <github@knappi.org>
2017-01-11 23:29:08 +0100

    fixup

--- .travis-ci/coverage.sh
+++ .travis-ci/coverage.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ "${TRAVIS_NODE_VERSION}" != 4.* ]] ; then
+if [[ "$( node --version ) " != v4.* ]] ; then
     # Remap coverage report back to typescript files
     npm install remap-istanbul
     # needs node 6 or later
-    remap-istanbul -i ./coverage/coverage.json -o ./coverage/coverage-remapped.json || true
-    istanbul report --include ./coverage/coverage-remapped.json text lcovonly || true
-    Send to coveralls
+    remap-istanbul -i ./coverage/coverage.json -o ./coverage/coverage-remapped.json || exit 1
+    istanbul report --include ./coverage/coverage-remapped.json text lcovonly || exit 1
+    # Send to coveralls
     npm install coveralls
-    cat ./coverage/lcov.info | coveralls
+    ( cat ./coverage/lcov.info | coveralls ) || exit 1
 fi
